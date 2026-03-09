d552776553ddb8748d72ebfd1ee46a6b51a13334
Francois Poizat <francois.poizat@gmail.com>
2026-03-09 13:46:13 +0100

    fix build to plt-posix.cpp

--- sdk/cpp/shared_library/build.sh
+++ sdk/cpp/shared_library/build.sh
@@ -2,6 +2,6 @@ clang++ -Wall -std=c++14 -fPIC -O3 -c ../HeliosDac.cpp
 clang++ -Wall -std=c++14 -fPIC -O3 -c HeliosDacAPI.cpp
 clang++ -Wall -std=c++14 -fPIC -O3 -c ../idn/idn.cpp
 clang++ -Wall -std=c++14 -fPIC -O3 -c ../idn/idnServerList.cpp
-clang -Wall -fPIC -O3 -c ../idn/plt-posix.c
+clang++ -Wall -std=c++14 -fPIC -O3 -c ../idn/plt-posix.cpp
 clang++ -shared -o libHeliosDACAPI.so HeliosDacAPI.o HeliosDac.o idn.o idnServerList.o plt-posix.o "../libusb_bin/Linux x64/libusb-1.0.so"
 
